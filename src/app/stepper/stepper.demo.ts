vmware-archive/clarity

chore: lint all js, json, and ts files

src/app/stepper/stepper.demo.ts
@@ -23,7 +23,7 @@ export class StepperDemo {
     console.log('reactive form submit', this.form.value);
   }
 
-  templateFormSubmit(templateFormValues: {}) {
+  templateFormSubmit(templateFormValues: any) {
     console.log('template form submit', templateFormValues);
   }
 

pulsating-radio-star/clr-angular-test

Signed-off-by: Steve Haar <info@stevehaar.com>

src/app/stepper/stepper.demo.ts
@@ -23,7 +23,7 @@ export class StepperDemo {
     console.log('reactive form submit', this.form.value);
   }
 
-  templateFormSubmit(templateFormValues: {}) {
+  templateFormSubmit(templateFormValues: unknown) {
     console.log('template form submit', templateFormValues);
   }
 